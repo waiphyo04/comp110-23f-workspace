d2d447e53c738641f568d10614491afa5c1cf96b
waiphyo <waiphyo@unc.edu>
2023-11-18 00:03:13 -0500

    commit

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -18,15 +18,21 @@ class Node:
         """Produce a string visualization of the linked list."""
         if self.next is None:
             # base case (where it ends!)
-            return f"{self.data} -> None"
+            return f"{self.data}"
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self):
-        return None
+    def head(self) -> int:
+        """head."""
+        return self.data
     
-    def tail(self):
-        return None
+    def tail(self) -> Node | None:
+        """Tail."""
+        return self.next
     
-    def last(self):
-        return None
+    def last(self) -> int:
+        """Last."""
+        if self.next is None:
+            return self.data
+        else:
+            return Node.last(self.next)
